398fae229e66ae1b78d965c3b9b93dfe9aaf0b3e
S. Andra Rifaldi <55216424+andrarif@users.noreply.github.com>
2020-01-28 22:49:46 +0700

    Update connection.php

--- web_service_api/connection.php
+++ web_service_api/connection.php
@@ -1,1 +1,17 @@
 
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "bioskop";
+
+$koneksi = mysqli_connect($server, $username, $password, $database);
+
+/* 
+if ($koneksi) {
+    echo "koneksi terhubung";
+} else {
+    echo "Database tidak tersedia";
+}
+*/
